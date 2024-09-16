7963d8a38ea0f4087a4339c5aad8c198910f21e0
Choclark <147842539+Choclark@users.noreply.github.com>
2024-09-16 18:29:16 +0100

    Update Navbar.tsx

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -22,17 +22,17 @@ const Navbar = () => {
                         Home
                     </li>
                 </Link>
-                <Link href={"/restaurant"}>
+                <Link href={"#"}>
                         <li className="hover:bg-slate-200 md:hover:bg-white md:hover:text-green-300 pl-3 rounded-md   py-2">
                             Restaurant
                         </li>
                 </Link>
-                <Link href={"/best-deal"}>
+                <Link href={"#"}>
                     <li className="hover:bg-slate-200 md:hover:bg-white md:hover:text-green-300 pl-3 rounded-md   py-2">
                         Best deals
                     </li>
                 </Link>
-                <Link href={"/faq"}>
+                <Link href={"#"}>
                     <li className="hover:bg-slate-200 md:hover:bg-white md:hover:text-green-300 pl-3 rounded-md   py-2">
                         FAQ
                     </li>
