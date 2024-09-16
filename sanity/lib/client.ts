cf50be68f3c50a3dd0c7b6d82a9e8b8c05abe05b
Choclark <147842539+Choclark@users.noreply.github.com>
2024-09-16 18:44:10 +0100

    Update client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  token:"sk8EDLFyDHl3UVwIDaL5vJMaieTahknb2CYe1mOZELBH1kMw9ilCIDLpi0Pyi8UNisulCCAQWWQpvebfLJhtP1Ouhy9z5M5ytbGpnSAXrtDDfnFTnqRlCFN8kbuKrrpHpAMYvSVTF2XEdg58XF9aHABpFv1neVtA0N7gaOcv7cTUbIxG863H",
+  token:"sk62EiuustGTSFyrQGhlVVREFU2tXrDUijaenHR3dSRELOPJ7im8brwQgMbNl662MxpLJEcRwCOYCpN0FMDEnP0KXhACs7TkmLtgeapSymz2H6nXU7jR8k1Zgg2dzSQcEuKdrwVWOWLZHQ8pkhKuH3ohEDh9R9tSBwrABivjTJZB8dliBfU0",
 })
